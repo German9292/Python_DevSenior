1ee127bcd20d9f81a5f415d569f36d1631ac7674
avila.9292@gmail.com <avila.9292@gmail.com>
2026-03-19 21:18:31 -0500

    Ejercicio de POO

---
+++ clases.py
@@ -0,0 +1,22 @@
+class Animales():
+    def __init__(self, nombre, raza, color,edad,genero):
+        self.nombre = nombre
+        self.raza = raza
+        self.color = color
+        self.edad = edad
+        self.genero = genero
+    def ladrar(self):
+        print('El perro ladra')
+    def correr(self):
+        print('El perro está corriendo')
+    def morder(self):
+        print('El perro muerde')
+    def __str__(self):
+        return (f"nombre: {self.nombre} raza: {self.raza} color: {self.color} edad: {self.edad} genero: {self.genero}")
+    
+perro_1 = Animales('lukas', 'pitbull', 'negro', '4', 'macho')
+perro_2 = Animales('Max', 'Pincher', 'blanco', '2', 'macho')
+print(perro_1)
+print(perro_2)
+
+
